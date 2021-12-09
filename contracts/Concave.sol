11b1418000cb60d8af74e7bcd62cadf756844271
zer0conf <25299353+zer0conf@users.noreply.github.com>
2021-12-09 06:40:47 +0200

    Update Concave.sol
    Simplify the withdrawal method

--- contracts/Concave.sol
+++ contracts/Concave.sol
@@ -99,27 +99,17 @@ contract ConcaveNFT is ERC721Enumerable, Pausable, Ownable {
         revealed = true;
     }
     function withdraw() public payable onlyOwner {
-        uint256 balance = address(this).balance;
-        uint256 for_treasury = balance * 60 / 100;
-        uint256 for_r1 = balance * 9 / 100;
-        uint256 for_r2 = balance * 5 / 100;
-        uint256 for_r3 = balance * 5 / 100;
-        uint256 for_r4 = balance * 5 / 100;
-        uint256 for_r5 = balance * 4 / 100;
-        uint256 for_r6 = balance * 4 / 100;
-        uint256 for_r7 = balance * 2 / 100;
-        uint256 for_r8 = balance * 4 / 100;
-        uint256 for_r9 = balance * 2 / 100;
-        payable(TREASURY).transfer(for_treasury);
-        payable(0x2F66d5D7561e1bE587968cd336FA3623E5792dc2).transfer(for_r1);
-        payable(0xeb9ADe429FBA6c51a1B352a869417Bd410bC1421).transfer(for_r2);
-        payable(0xf1A1e46463362C0751Af4Ff46037D1815d66bB4D).transfer(for_r3);
-        payable(0x1E3005BD8281148f1b00bdcC94E8d0cD9DA242C2).transfer(for_r4);
-        payable(0x21761978a6F93D0bF5bAb5F75762880E8dc813e8).transfer(for_r5);
-        payable(0x5b3DBf9004E01509777329B762EC2332565F12fA).transfer(for_r6);
-        payable(0xB2b9FF858Bf74436685DaaF76d6901C2A24ef0C3).transfer(for_r7);
-        payable(0xe873Fa8436097Bcdfa593EEd60c10eFAd4244dC0).transfer(for_r8);
-        payable(0x182e0C610c4A855b81169385821C4c8690Af5f3b).transfer(for_r9);
+        uint currentBalance = address(this).balance;
+        payable(TREASURY).transfer(currentBalance * 60 / 100);
+        payable(0x2F66d5D7561e1bE587968cd336FA3623E5792dc2).transfer(currentBalance * 9 / 100); // md
+        payable(0xeb9ADe429FBA6c51a1B352a869417Bd410bC1421).transfer(currentBalance * 5 / 100); // rd
+        payable(0xf1A1e46463362C0751Af4Ff46037D1815d66bB4D).transfer(currentBalance * 5 / 100); // br
+        payable(0x1E3005BD8281148f1b00bdcC94E8d0cD9DA242C2).transfer(currentBalance * 5 / 100); // wt
+        payable(0x21761978a6F93D0bF5bAb5F75762880E8dc813e8).transfer(currentBalance * 4 / 100); // mt
+        payable(0x5b3DBf9004E01509777329B762EC2332565F12fA).transfer(currentBalance * 4 / 100); // sh
+        payable(0xB2b9FF858Bf74436685DaaF76d6901C2A24ef0C3).transfer(currentBalance * 2 / 100); // a1
+        payable(0xe873Fa8436097Bcdfa593EEd60c10eFAd4244dC0).transfer(currentBalance * 4 / 100); // nu
+        payable(0x182e0C610c4A855b81169385821C4c8690Af5f3b).transfer(currentBalance * 2 / 100); // y1
     }
     function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
         notRevealedUri = _notRevealedURI;
