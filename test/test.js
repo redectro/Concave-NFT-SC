38dc04512765a2961b74ee2022ca982e01adfdf9
jbtobar <jbtobar.io@gmail.com>
2021-12-09 15:56:27 -0500

    constants test

--- test/test.js
+++ test/test.js
@@ -5,6 +5,10 @@ const _name = 'ConcaveName';
 const _symbol = 'ConcaveSymbol';
 const _initBaseURI = 'Base URI';
 const _initNotRevealedUri = 'Unrevealed Base URI';
+const maxMintAmount = 10;
+const price_in_ether = 0.04;
+const price = ethers.utils.parseEther(price_in_ether.toString());
+const revealed = false;
 
 describe("ConcaveNFT", () => {
     let ConcaveNFT;
@@ -20,17 +24,26 @@ describe("ConcaveNFT", () => {
         );
     })
 
-    it(`name is ${_name}`, async () => {
-        await concavenft.name();
+    it(`name is "${_name}"`, async () => {
+        expect(await concavenft.name()).to.equal(_name)
     })
-    it(`symbol is ${_symbol}`, async () => {
-        await concavenft.symbol();
+    it(`symbol is "${_symbol}"`, async () => {
+        expect(await concavenft.symbol()).to.equal(_symbol)
     })
-    it(`baseURI is ${_initBaseURI}`, async () => {
-        await concavenft.baseURI();
+    it(`baseURI is "${_initBaseURI}"`, async () => {
+        expect(await concavenft.baseURI()).to.equal(_initBaseURI)
     })
-    it(`notRevealedUri is ${_initNotRevealedUri}`, async () => {
-        await concavenft.notRevealedUri();
+    it(`notRevealedUri is "${_initNotRevealedUri}"`, async () => {
+        expect(await concavenft.notRevealedUri()).to.equal(_initNotRevealedUri)
+    })
+    it(`maxMintAmount is "${maxMintAmount}"`, async () => {
+        expect(await concavenft.maxMintAmount()).to.equal(maxMintAmount)
+    })
+    it(`price is "${ethers.utils.formatEther(price)}"`, async () => {
+        expect(await concavenft.price()).to.equal(price)
+    })
+    it(`revealed is "${revealed}"`, async () => {
+        expect(await concavenft.revealed()).to.equal(revealed)
     })
 
 
